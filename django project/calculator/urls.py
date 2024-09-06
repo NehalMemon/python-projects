1ca45814c03e9ba02d264cb8a6e3c849283b11b8
NehalMemon <154071394+NehalMemon@users.noreply.github.com>
2024-09-06 21:44:58 +0500

    Delete django project/calculator directory

--- django project/calculator/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path("total/", views.add, name='total'),
-    path("avg/", views.avg, name='average'),
-    path("pro/", views.product, name='production'),
-]
